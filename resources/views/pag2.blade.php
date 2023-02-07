551d9094b7a10f9b0937c0d7690b6e9bf1dd7184
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-02-07 16:43:52 +0100

    add home link and done bonus1

--- resources/views/pag2.blade.php
+++ resources/views/pag2.blade.php
@@ -5,12 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>first laravel</title>
+    <title>pagina2</title>
 </head>
 
 <body>
-    <h1>Home Laravel</h1>
-    <a href="pag2">link pagina 2</a>
+    <h1>
+        questa è la pagina 2 di Laravel
+    </h1>
+    <a href="/">Ritorna alla home page</a>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/pag2', function () {
+    return view('pag2');
+});
